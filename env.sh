26f96ef821ff28d3236402ab3e2932c8f69ec7ac
Bill Harris <relicx74@gmail.com>
2022-09-07 23:06:38 -0700

    Deploy updates

---
+++ env.sh
@@ -0,0 +1,7 @@
+#Make sure to call this with source env.sh for things to stick
+export DATABASE_URL=http://fake.com
+export NEXTAUTH_SECRET=bleh
+export DISCORD_CLIENT_ID=blah
+export DISCORD_CLIENT_SECRET=blah
+export NEXTAUTH_URL=http://fake.com
+export PORT=8080

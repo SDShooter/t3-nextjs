90d4e789de1ca1154a1dca9a2efcaf51d10a6219
relicx74 <relicx74@gmail.com>
2022-09-08 19:55:19 -0700

    chain commands

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-bun add -d bun-framework-next
-echo "added bun-framework-next"
-bun install --verbose
-echo "bun installed"
-bun bun --use next
-echo "bun bun done"
-npx tailwindcss -c ./tailwind.config.cjs -i ./src/styles/globals.css -o ./public/styles/globals.css
-echo "npx tailwind done"
+bun add -d bun-framework-next && \
+echo "added bun-framework-next" && \
+bun install --verbose && \
+echo "bun installed" && \
+bun bun --use next && \
+echo "bun bun done" && \
+npx tailwindcss -c ./tailwind.config.cjs -i ./src/styles/globals.css -o ./public/styles/globals.css && \
+echo "npx tailwind done" && \
 bun build
 echo "bun build done"

5f2d4e09fe55e08a78746bb617038065e37916ab
relicx74 <relicx74@gmail.com>
2022-09-10 21:05:10 -0700

    Took out tRPC call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  //const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
     <>
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
           />
         </div>
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+          {/* {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>} */}
         </div>
       </main>
     </>

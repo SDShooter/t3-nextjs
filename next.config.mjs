b773bd9177e7bdd26ddc87af8d0c024fa864c828
relicx74 <relicx74@gmail.com>
2022-09-15 00:58:53 -0700

    New approach to dockerfile using next optimized server build

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
   //eslint: { ignoreDuringBuilds: true },
   //typescript: { ignoreBuildErrors: true },
 });
